5f5500b72024db6991b0446ee6713ba87f4f2e65
feng19980910 <feng19980910@foxmail.com>
2018-07-17 12:58:05 +0800

    debug the InterCall in Runoob

--- Runoob/InterCall/WithoutPublicClass.java
+++ Runoob/InterCall/WithoutPublicClass.java
@@ -1,5 +1,9 @@
-class cla {
-    static void main(String [] args) {
+class WithoutPublicClass {
+    public static void main(String [] args) {
         System.out.println("Hello, World!");
+        InterCallA a = new InterCallA();
+        InterCallB b = new InterCallB();
+        a.show();
+        b.show();
     }
 }
